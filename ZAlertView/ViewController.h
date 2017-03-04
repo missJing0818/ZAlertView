b32de43b5d84e68ae0a9ee70bd18d302a14a9e08
missJing0818 <978015832@qq.com>
2017-03-04 10:07:56 +0800

    Initial Commit

---
+++ ZAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZAlertView
+//
+//  Created by renmiantaohua on 17/3/4.
+//  Copyright © 2017年 zhangjing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZAlertView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZAlertView
+//
+//  Created by renmiantaohua on 17/3/4.
+//  Copyright © 2017年 zhangjing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
